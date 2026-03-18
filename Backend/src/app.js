3fd46eb3901414ace4f3c961cf19719552bdff10
Aneek <aneeksaha2015@gmail.com>
2026-03-19 02:39:57 +0530

    final of final change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,5 +21,14 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api/auth', authRoutes)
 app.use('/api/chat', chatRoutes)
 
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
 
 module.exports = app;
